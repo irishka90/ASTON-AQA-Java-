9b17e7390f1b2b7b50f90035ca738bb145328540
irishka90 <irishka.131290@gmail.com>
2023-11-13 22:41:53 +0300

    lesson 3

--- ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
@@ -2,8 +2,14 @@ package org.aston.school.lesson1;
 
 public class MainApp {
     public static void main(String[] args) {
-
+        System.out.println(5);
     }
 
-
+    private static boolean isBetween10_20(int a, int b) {
+        if (a + b >= 10 && a + b <= 20) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
