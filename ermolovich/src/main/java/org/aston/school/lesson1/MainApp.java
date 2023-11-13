4bad7e0c732770385e89033a18cd2229059a8ad0
irishka90 <irishka.131290@gmail.com>
2023-11-13 23:29:18 +0300

    lesson 3

--- ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
@@ -2,14 +2,29 @@ package org.aston.school.lesson1;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println(5);
+        // System.out.println(isBetween10_20(11, 5)); //ex.1
+        isPositiveOrNot(-12);                     //ex.2
+      //  System.out.println(isPositive(11));       //ex.3
+
     }
 
+    //ex.1
     private static boolean isBetween10_20(int a, int b) {
-        if (a + b >= 10 && a + b <= 20) {
-            return true
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    //ex.2
+    private static void isPositiveOrNot(int a) {
+        // System.out.println(a >= 0 ? "Положительное число" : "Отрицательное число");
+        if (a >= 0) {
+            System.out.println("Положительное число");
         } else {
-            return false
+            System.out.println("Отрицательное число");
         }
+    }
+
+    //ex.3
+    private static boolean isPositive(int a) {
+        return a < 0;
     }
 }
