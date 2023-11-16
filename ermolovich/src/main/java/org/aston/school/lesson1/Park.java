34e9a0a39faf4bfa800f073f4ffccc5cdd1330a5
irishka90 <irishka.131290@gmail.com>
2023-11-16 22:00:25 +0300

    lesson 4

---
+++ ermolovich/src/main/java/org/aston/school/lesson1/Park.java
@@ -0,0 +1,14 @@
+package org.aston.school.lesson1;
+
+public class Park {
+
+    private String name;
+    private Attraction [];
+
+    public class Attraction {
+        private String name;
+        private int time;
+        private int price;
+
+    }
+}
