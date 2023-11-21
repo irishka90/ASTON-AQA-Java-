bb5a40ed154509aab05beae6400c5721e8e99f94
irishka90 <irishka.131290@gmail.com>
2023-11-21 14:39:48 +0300

    lesson 5

---
+++ ermolovich/src/main/java/org/aston/school/lesson1/Animal.java
@@ -0,0 +1,13 @@
+package org.aston.school.lesson1;
+
+public class Animal {String name;
+    int swim;
+    int run;
+    public Animal(String name,int swim,int run){
+        this.name=name;
+        this.swim=swim;
+        this.run=run;
+    }
+
+
+}
