ede1cba34f845c71890d6cf4bac9639ac45e0d22
irishka90 <irishka.131290@gmail.com>
2023-11-21 20:16:54 +0300

    lesson 3

--- ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
-         System.out.println(isBetween10_20(11, 5)); //ex.1
+        System.out.println(isBetween10_20(11, 5)); //ex.1
 
         manyStrings(2, "Hello");            //ex.2
 
@@ -14,8 +14,8 @@ public class MainApp {
         System.out.println(isPositive(11));       //ex.4
 
 
+        System.out.println(leapYear(2204));          //ex.5
 
-        // System.out.println(leapYear(2204));          //ex.5
 
         int[] arr = {1, 0, 0, 1, 0, 0, 1, 0};       //ex.6
         masiveChange(arr);
@@ -69,13 +69,12 @@ public class MainApp {
     // если число отрицательное, и вернуть false если положительное.
 
     private static boolean isPositive(int a) {
-        return a <0;
+        return a < 0;
     }
 
 
-
-
-    //ex.5
+    //ex.5 Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, не
+    //високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
     private static boolean leapYear(int a) {
         if (a % 4 == 0) {
             return a % 400 == 0 || a % 100 != 0;
@@ -83,7 +82,8 @@ public class MainApp {
         return false;
     }
 
-    //ex.6
+    //ex.6 Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и
+    //условия заменить 0 на 1, 1 на 0;
     private static void masiveChange(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
@@ -99,7 +99,7 @@ public class MainApp {
 
     }
 
-    //ex.7
+    //ex.7 Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;
     private static void masive100() {
         int[] arr = new int[100];
 
@@ -112,7 +112,7 @@ public class MainApp {
 
     }
 
-    //ex.8
+    //ex.8 Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
     private static void less6Masiv(int[] mas) {
 
         for (int i = 0; i < mas.length; i++) {
@@ -123,7 +123,10 @@ public class MainApp {
         System.out.println(Arrays.toString(mas));
     }
 
-    //ex.9
+    //ex.9 Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
+    //цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
+    //Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то
+    //есть [0][0], [1][1], [2][2], ..., [n][n];
     private static void tableMasiv(int n) {
         int[][] table = new int[n][n];
 
@@ -144,7 +147,8 @@ public class MainApp {
         }
     }
 
-    //ex.10
+    //ex.10 Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив типа int
+    //длиной len, каждая ячейка которого равна initialValue.
     private static void lineMasiv(int len, int initialValue) {
         int[] lineMas = new int[len];
         for (int i = 0; i < len; i++) {
