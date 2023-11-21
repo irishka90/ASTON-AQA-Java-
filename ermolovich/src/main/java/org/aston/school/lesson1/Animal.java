21369dc42c2cf24e2abc055268df0199d9125fec
irishka90 <irishka.131290@gmail.com>
2023-11-21 14:47:41 +0300

    lesson 5

--- ermolovich/src/main/java/org/aston/school/lesson1/Animal.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/Animal.java
@@ -1,12 +1,23 @@
 package org.aston.school.lesson1;
 
-public class Animal {String name;
-    int swim;
-    int run;
-    public Animal(String name,int swim,int run){
-        this.name=name;
-        this.swim=swim;
-        this.run=run;
+public class Animal {
+    private String name;
+    private int swim;
+    private int run;
+
+    public Animal(String name, int swim, int run) {
+        this.name = name;
+        this.swim = swim;
+        this.run = run;
+    }
+
+    public class Cat extends Animal {
+        private String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
     }
 
 
