9e37c1c80a7ffe32b5a62e4582b71165de732fa1
irishka90 <irishka.131290@gmail.com>
2023-12-04 23:57:14 +0300

    lesson 9

--- ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
@@ -1,11 +1,9 @@
 package org.aston.school.lesson1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class MainApp {
 
@@ -13,6 +11,7 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Количество четных:" + countEvenNumbers());// 1.
         collectionHigh();  //2.
+        sortUp();   //3.
     }
 
     private static int countEvenNumbers() {
@@ -36,13 +35,8 @@ public class MainApp {
 
     }
 
-    //2. Задана коллекция, состоящая из строк: «Highload», «High», «Load», «Highload». Нужно с ней выполнить следующие манипуляции:
-    //2.1. Посчитать, сколько раз объект «High» встречается в коллекции;
-    //2.2. Определить, какой элемент в коллекции находится на первом месте. Если мы получили пустую коллекцию, то пусть возвращается 0;
-    //2.3. Необходимо вернуть последний элемент, если получили пустую коллекцию, то пусть возвращается 0;
-    private static void collectionHigh() {
 
-        List<String> high = Arrays.asList("Highload", "High", "Load", "LoadHigh"); //Коллекция исходных значений
+    private static List<String> generateList(List<String> original) { //original - Коллекция исходных значений
         Random random = new Random();
 
         Function<Integer, List<String>> listGenerator = collectionSize -> {
@@ -50,15 +44,24 @@ public class MainApp {
             ArrayList<String> newHigh = new ArrayList<>(); //Создание коллекции из случайной комбинации из исходных значений
 
             for (int i = 0; i < collectionSize; i++) {
-                int position = random.nextInt(high.size()); //выбираем случайную позицию в исходной коллекции
-                String word = high.get(position); // получаем значение по позиции
+                int position = random.nextInt(original.size()); //выбираем случайную позицию в исходной коллекции
+                String word = original.get(position); // получаем значение по позиции
                 newHigh.add(word);  //добавляем его в новую коллекцию
             }
             return newHigh;
         };
 
         int collectionSize = random.nextInt(20); //задаем случайный размер (до 20 слов)
-        List<String> newHigh = listGenerator.apply(collectionSize);
+        return listGenerator.apply(collectionSize);
+    }
+
+    //2. Задана коллекция, состоящая из строк: «Highload», «High», «Load», «Highload». Нужно с ней выполнить следующие манипуляции:
+    //2.1. Посчитать, сколько раз объект «High» встречается в коллекции;
+    //2.2. Определить, какой элемент в коллекции находится на первом месте. Если мы получили пустую коллекцию, то пусть возвращается 0;
+    //2.3. Необходимо вернуть последний элемент, если получили пустую коллекцию, то пусть возвращается 0;
+    private static void collectionHigh() {
+
+        List<String> newHigh = generateList(Arrays.asList("Highload", "High", "Load", "LoadHigh"));
         System.out.println(newHigh); //новая коллекция
 
         //2.1
@@ -85,6 +88,26 @@ public class MainApp {
 
         System.out.println("первое слово: " + action2.getItem(newHigh, true));
         System.out.println("последнее слово: " + action2.getItem(newHigh, false));
+    }
+
+
+    //3. Задана коллекция, содержащая элементы "f10", "f15", "f2", "f4", "f4".
+    // Необходимо отсортировать строки по возрастанию и добавить их в массив;
+    private static void sortUp() {
+        List<String> newNotSorted = generateList(Arrays.asList("f10", "f15", "f2", "f4"));
+        System.out.println(newNotSorted); //новая коллекция
+
+        UnaryOperator<List<String>> action = notSortedList -> {
+            notSortedList.sort(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareTo(o2);
+                }
+            });
+            return notSortedList;
+        };
+
+        System.out.println(action.apply(newNotSorted)); //отсортированная коллекция
     }
 
 
