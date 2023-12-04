e4f15d6e3bc0a1053d07de20a76dc68bb998cbae
irishka90 <irishka.131290@gmail.com>
2023-12-04 23:30:37 +0300

    lesson 9

---
+++ ermolovich/src/main/java/org/aston/school/lesson1/HighWord.java
@@ -0,0 +1,8 @@
+package org.aston.school.lesson1;
+
+import java.util.List;
+
+public interface HighWord {
+    String getItem (List<String> highWords, boolean firstOrLast);
+
+}
--- ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
+++ ermolovich/src/main/java/org/aston/school/lesson1/MainApp.java
@@ -1,56 +1,90 @@
 package org.aston.school.lesson1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class MainApp {
+
+
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        System.out.println("Количество четных:" + countEvenNumbers());// 1.
+        collectionHigh();  //2.
     }
 
-    private static void printThreeWords() {
-        System.out.print("Orange\nBanana\nApple\n");
-    }
+    private static int countEvenNumbers() {
+        //1. Для любого набора случайно-сгенерированных чисел нужно определить количество чётных чисел.
 
-    // или 2й, более длинный вариант
-    private static void printThreeWords1() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
+        Predicate<Integer> countEvenFunc = (n) -> n % 2 == 0;
 
+        Random random = new Random();    // Создаю массив случайных чисел, длинна массива до 12 штук
+        int[] nums = new int[random.nextInt(12)];
 
-    private static void checkSumSign() {
-        int a = 0;
-        int b = -10;
-        if (a + b >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+        int counterEven = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt();
+            if (countEvenFunc.test(nums[i])) {
+                counterEven++;
+            }
         }
-    }
-
-    private static void printColor() {
-        int value = 120;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 100) {
-            System.out.println("Зеленый");
-        } else {
-            System.out.println("Желтый");
-
-        }
+        System.out.println(Arrays.toString(nums));
+        return counterEven;
 
     }
 
-    private static void compareNumbers() {
-        int a = 120;
-        int b = 17;
-        if (a >= b) {
-            System.out.println("a>=b");
-        } else {
-            System.out.println("a<b");
-        }
+    //2. Задана коллекция, состоящая из строк: «Highload», «High», «Load», «Highload». Нужно с ней выполнить следующие манипуляции:
+    //2.1. Посчитать, сколько раз объект «High» встречается в коллекции;
+    //2.2. Определить, какой элемент в коллекции находится на первом месте. Если мы получили пустую коллекцию, то пусть возвращается 0;
+    //2.3. Необходимо вернуть последний элемент, если получили пустую коллекцию, то пусть возвращается 0;
+    private static void collectionHigh() {
+
+        List<String> high = Arrays.asList("Highload", "High", "Load", "LoadHigh"); //Коллекция исходных значений
+        Random random = new Random();
+
+        Function<Integer, List<String>> listGenerator = collectionSize -> {
+
+            ArrayList<String> newHigh = new ArrayList<>(); //Создание коллекции из случайной комбинации из исходных значений
+
+            for (int i = 0; i < collectionSize; i++) {
+                int position = random.nextInt(high.size()); //выбираем случайную позицию в исходной коллекции
+                String word = high.get(position); // получаем значение по позиции
+                newHigh.add(word);  //добавляем его в новую коллекцию
+            }
+            return newHigh;
+        };
+
+        int collectionSize = random.nextInt(20); //задаем случайный размер (до 20 слов)
+        List<String> newHigh = listGenerator.apply(collectionSize);
+        System.out.println(newHigh); //новая коллекция
+
+        //2.1
+        Function<List<String>, Integer> action1 = highWords -> {
+            int counter = 0;  //подсчет вхождений High в новой коллекции
+            for (String word : highWords) {
+                if (word.contains("High")) {
+                    counter++;
+                }
+            }
+            return counter;
+        };
+
+        System.out.println("High кол-во:" + action1.apply(newHigh));
+
+        //2.2-2.3
+        HighWord action2 = (highWords, firstOrLast) -> {
+            if (highWords.isEmpty()) return "0";
+
+            if (firstOrLast) return highWords.get(0);
+
+            return highWords.get(highWords.size() - 1);
+        };
+
+        System.out.println("первое слово: " + action2.getItem(newHigh, true));
+        System.out.println("последнее слово: " + action2.getItem(newHigh, false));
     }
 
 
