5049d21f0920a58e83a9ac454b62f77a0a964254
irishka90 <irishka.131290@gmail.com>
2023-12-21 15:46:49 +0300

    Lesson_12_testj

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -2,7 +2,7 @@ package org.aston;
 
 // Он по сути не нужен только для проверки себя
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.print(Fact.getFactorial(4));
     }
 
--- src/test/java/AstonJUnitTests.java
+++ src/test/java/AstonJUnitTests.java
@@ -4,6 +4,7 @@
 //import org.junit.jupiter.api.Test;
 
 import org.aston.Fact;
+import org.aston.FactorialException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +12,34 @@ import org.junit.jupiter.api.Test;
 public class AstonJUnitTests {
     @Test
     @DisplayName("Нахождение факториала")
-    public void Factorial() {
+    public void factorial1() throws Exception {
         System.out.println("======TEST  fact 4 =======");
         Assertions.assertEquals(24, Fact.getFactorial(4), "Errror");
     }
+
+    @Test()
+    public void factorial2() throws Exception {
+        System.out.println("======TEST  fact -1 =======");
+
+        Exception exception = Assertions.assertThrows(FactorialException.class, () -> {
+            Fact.getFactorial(-1);
+        });
+
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains("Значение должно быть от 0 до 20"));
+    }
+
+    @Test()
+    public void factorial3() throws Exception {
+        System.out.println("======TEST  fact 21 =======");
+
+        Exception exception = Assertions.assertThrows(FactorialException.class, () -> {
+            Fact.getFactorial(21);
+        });
+
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains("Значение должно быть от 0 до 20"));
+    }
+
 
 }
