a7b5d6129aa6aff71eb2d22b008b768ebedfd582
irishka90 <irishka.131290@gmail.com>
2023-12-26 21:34:20 +0300

    Lesson_13

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -17,12 +17,13 @@ public class AppTest {
     @BeforeAll
     static void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://www.mts.by/");
     }
 
-    @BeforeAll
-    static void applyCookies() {
+    @BeforeEach
+    void open() {
+        driver = new ChromeDriver();
+        driver.get("https://www.mts.by/");
+
         WebElement applyCookies = driver.findElement(new By.ByXPath("//*[@id=\"cookie-agree\"]"));
         applyCookies.click();
     }
@@ -30,14 +31,14 @@ public class AppTest {
     @Test
     @DisplayName("Проверить название указанного блока")
     void test1() {
-        WebElement titleName = driver.findElement(new By.ByXPath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2"));
+        WebElement titleName = driver.findElement(By.className("pay__wrapper")).findElement(By.tagName("h2"));
         Assertions.assertEquals("Онлайн пополнение\nбез комиссии", titleName.getText());
     }
 
     @Test
     @DisplayName("Проверить наличие логотипов платёжных систем")
     void test2() {
-        WebElement logos = driver.findElement(new By.ByXPath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul"));
+        WebElement logos = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul"));
         List<WebElement> elements = logos.findElements(By.tagName("li"));
         Assertions.assertNotEquals(elements.size(), 0);
     }
@@ -45,12 +46,17 @@ public class AppTest {
     @Test
     @DisplayName("Проверить работу ссылки «Подробнее о сервисе»")
     void test3() {
-        WebElement link = driver.findElement(new By.ByXPath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/a"));
+        WebElement link = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/a"));
         String linkHref = link.getAttribute("href");
         link.click();
 
         new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.urlContains(linkHref));
         Assertions.assertEquals(linkHref, driver.getCurrentUrl());
+
+        WebElement container = driver.findElement(By.className("main"));
+        WebElement header = container.findElement(By.className("container-fluid"));
+
+        Assertions.assertFalse(header.getText().isEmpty());
     }
 
     //Заполнить поля и проверить работу кнопки «Продолжить»
@@ -62,30 +68,30 @@ public class AppTest {
         driver.get("https://www.mts.by/");
 
 
-        WebElement phone = driver.findElement(new By.ById("connection-phone"));
+        WebElement phone = driver.findElement(By.id("connection-phone"));
         phone.click();
         phone.sendKeys("297777777");
-        WebElement sum = driver.findElement(new By.ById("connection-sum"));
+        WebElement sum = driver.findElement(By.id("connection-sum"));
         sum.click();
         sum.sendKeys("29");
-        WebElement button = driver.findElement(new By.ByXPath("//*[@id=\"pay-connection\"]/button"));
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
         button.click();
 
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(new By.ByCssSelector(".bepaid-iframe")));
-        WebElement paidFrame = driver.findElement(new By.ByCssSelector(".bepaid-iframe"));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".bepaid-iframe")));
+        WebElement paidFrame = driver.findElement(By.cssSelector(".bepaid-iframe"));
 
         driver.switchTo().frame(paidFrame);
 
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/button")));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/button")));
 
-        WebElement buttonPay = driver.findElement(new By.ByXPath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/button"));
+        WebElement buttonPay = driver.findElement(By.tagName("button"));
 
 
         Assertions.assertTrue(buttonPay.isDisplayed());
     }
 
-    @AfterAll
-    static void close() {
+    @AfterEach
+    void close() {
         driver.quit();
     }
 
