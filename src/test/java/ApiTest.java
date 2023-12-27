731d892b06635d91f3de60af5c50912a9dcc32d7
irishka90 <irishka.131290@gmail.com>
2023-12-27 17:51:56 +0300

    Lesson_16

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -5,6 +5,9 @@
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.core.IsNull;
+import org.hamcrest.core.IsSame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,31 +21,60 @@ public class ApiTest {
 
     private final String BASE_URL = "https://postman-echo.com";
 
+    private final Map<String, String> paramsEmpty = new HashMap<>();
+
 
     @Test
     @DisplayName("GET test")
-    public void test1() {
+    public void test() {
 
         RestAssured.given()
                 .baseUri(BASE_URL)
+                .accept(ContentType.ANY)
                 .when()
-                .queryParam("foo1", "bar1")
-                .queryParam("foo2", "bar2")
                 .get("get")
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .assertThat()
-                .body("url", startsWith("https://postman-echo.com/get"))
+                .body("url", equalTo(BASE_URL + "/get"))
                 .body("headers.x-amzn-trace-id", startsWith("Root="))
                 .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.x-forwarded-port", equalTo("443"))
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.accept-encoding", equalTo("gzip,deflate"))
                 .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("args.foo1", equalTo("bar1"))
-                .body("args.foo2", equalTo("bar2"));
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()))
+                .body("args", equalTo(paramsEmpty));
+    }
+
+    @Test
+    @DisplayName("GET with query test")
+    public void test1() {
+
+        Map<String, String> params = new HashMap<>();
+        params.put("foo1", "bar1");
+        params.put("foo2", "bar2");
+
+        RestAssured.given()
+                .baseUri(BASE_URL)
+                .accept(ContentType.ANY)
+                .when()
+                .queryParams(params)
+                .get("get")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .assertThat()
+                .body("url", startsWith(BASE_URL + "/get"))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()))
+                .body("args", equalTo(params));
     }
 
 
@@ -50,28 +82,33 @@ public class ApiTest {
     @DisplayName("POST raw test")
     public void test2() {
 
-        Map<String, String> params = new HashMap<>();
-        params.put("test", "value");
+        String json = "{\n" +
+                "    \"test\": \"value\"\n" +
+                "}";
 
         RestAssured.given()
                 .baseUri(BASE_URL)
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .params(params)
+                .accept(ContentType.ANY)
+                .contentType(ContentType.TEXT)
+                .body(json)
                 .post("post")
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .assertThat()
-                .body("url", equalTo("https://postman-echo.com/post"))
-                .body("data", equalTo("test=value"))
+                .body("url", equalTo(BASE_URL + "/post"))
+                .body("data", equalTo(json))
+                .body("files", equalTo(paramsEmpty))
+                .body("form", equalTo(paramsEmpty))
+                .body("args", equalTo(paramsEmpty))
+                .body("json", IsNull.nullValue())
                 .body("headers.x-amzn-trace-id", startsWith("Root="))
                 .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.x-forwarded-port", equalTo("443"))
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.accept-encoding", equalTo("gzip,deflate"))
                 .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
-                .body("headers.accept", equalTo(ContentType.JSON.getAcceptHeader()));
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()));
     }
 
 
@@ -79,32 +116,129 @@ public class ApiTest {
     @DisplayName("POST form test")
     public void test3() {
 
-        String json = "{\n" +
-                "        \"foo1\": \"bar1\",\n" +
-                "        \"foo2\": \"bar2\"\n" +
-                "    }";
+        Map<String, String> params = new HashMap<>();
+        params.put("foo1", "bar1");
+        params.put("foo2", "bar2");
 
         RestAssured.given()
                 .baseUri(BASE_URL)
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .body(json)
+                .accept(ContentType.ANY)
+                .contentType(ContentType.URLENC.withCharset("UTF-8"))
+                .params(params)
+                .request()
                 .post("post")
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .assertThat()
-                .body("url", equalTo("https://postman-echo.com/post"))
-                .body("json", equalTo(json))
+                .body("url", equalTo(BASE_URL + "/post"))
+                .body("form", equalTo(params))
+                .body("json", equalTo(params))
+                .body("files", equalTo(paramsEmpty))
+                .body("args", equalTo(paramsEmpty))
+                .body("data", new IsSame<String>(""))
                 .body("headers.x-amzn-trace-id", startsWith("Root="))
                 .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.x-forwarded-port", equalTo("443"))
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.accept-encoding", equalTo("gzip,deflate"))
                 .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
-                .body("headers.accept", equalTo(ContentType.JSON.getAcceptHeader()));
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()));
     }
 
+    @Test
+    @DisplayName("PUT test")
+    public void test4() {
+
+        String data = "This is expected to be sent back as part of response body.";
+
+        RestAssured.given()
+                .baseUri(BASE_URL)
+                .accept(ContentType.ANY)
+                .contentType(ContentType.TEXT)
+                .body(data)
+                .put("put")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .assertThat()
+                .body("url", equalTo(BASE_URL + "/put"))
+                .body("data", equalTo(data))
+                .body("json", IsNull.nullValue())
+                .body("files", equalTo(paramsEmpty))
+                .body("form", equalTo(paramsEmpty))
+                .body("args", equalTo(paramsEmpty))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()));
+    }
+
+
+    @Test
+    @DisplayName("PATCH test")
+    public void test5() {
+
+        String data = "This is expected to be sent back as part of response body.";
+
+        RestAssured.given()
+                .baseUri(BASE_URL)
+                .accept(ContentType.ANY)
+                .contentType(ContentType.TEXT)
+                .body(data)
+                .patch("patch")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .assertThat()
+                .body("url", equalTo(BASE_URL + "/patch"))
+                .body("data", equalTo(data))
+                .body("json", IsNull.nullValue())
+                .body("files", equalTo(paramsEmpty))
+                .body("form", equalTo(paramsEmpty))
+                .body("args", equalTo(paramsEmpty))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()));
+    }
+
+    @Test
+    @DisplayName("DELETE test")
+    public void test6() {
+
+        String data = "This is expected to be sent back as part of response body.";
+
+        RestAssured.given()
+                .baseUri(BASE_URL)
+                .accept(ContentType.ANY)
+                .contentType(ContentType.TEXT)
+                .body(data)
+                .delete("delete")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .assertThat()
+                .body("url", equalTo(BASE_URL + "/delete"))
+                .body("data", equalTo(data))
+                .body("json", IsNull.nullValue())
+                .body("files", equalTo(paramsEmpty))
+                .body("form", equalTo(paramsEmpty))
+                .body("args", equalTo(paramsEmpty))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/17.0.9)"))
+                .body("headers.accept", equalTo(ContentType.ANY.getAcceptHeader()));
+    }
 
 
 }
